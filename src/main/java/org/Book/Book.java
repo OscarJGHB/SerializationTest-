a6b04293f83d897130a67fbe4dff98b50d058a9b
OscarJGHB <Oscaar.Jiang@outlook.com>
2025-05-05 02:39:57 -0400

    added a bunch of tests and fixed up serialize and deserialize

--- src/main/java/org/Book/Book.java
+++ src/main/java/org/Book/Book.java
@@ -1,6 +1,8 @@
 package org.Book;
 
+import javax.xml.transform.stream.StreamSource;
 import java.io.*;
+import java.nio.file.Files;
 import java.util.Scanner;
 
 
@@ -18,15 +20,18 @@ public class Book
 
     public Book(String title, String author, String genre, int year)
     {
-        this.title = title;
-        this.author = author;
-        this.genre = genre;
+        this.title = title.trim();
+        this.author = author.trim();
+        this.genre = genre.trim();
         this.year = year;
 
 
     }
 
 
+    public static String getFormat(){
+        return csvHeader;
+    }
 
     public static void serialize(Book book, File file)
     {
@@ -51,13 +56,13 @@ public class Book
         }
         catch(IOException e) {
 
-            System.out.println("Error creating file");
+            System.out.println("Error accessing file");
             return;
         }
 
 
         try(Scanner scnr = new Scanner(file)){
-            boolean emptyOrInvalid = (file.length() == 0 || !(scnr.hasNextLine() && scnr.nextLine().equals(Book.csvHeader)));
+            boolean emptyOrInvalid = (file.length() == 0 || !(scnr.hasNextLine() && scnr.nextLine().trim().equals(Book.csvHeader.trim())));
             try(FileWriter fw = new FileWriter(file, !emptyOrInvalid))
             {
                 if(emptyOrInvalid)
@@ -76,17 +81,30 @@ public class Book
 
     public static Book deserialize(File file)
     {
-        if(dsFile == null || !file.getAbsolutePath().equals(dsFile.getAbsolutePath()))
-        {
-            if(!(file.exists() && file.isFile() && file.getName().endsWith(".csv") && file.length() != 0)) {
-                System.out.printf("File \"%10s\" is invalid to deserialize\n", file.getName());
-                return null;
+
+        //tests to see if we already have a file
+        try {
+            if (dsFile == null || (Files.isSameFile(dsFile.toPath(), file.toPath()))) {
+                //checks validity
+                if (!(file.exists() && file.isFile() && file.getName().endsWith(".csv") && file.length() != 0)) {
+                    System.out.printf("File \"%10s\" is invalid to deserialize\n", file.getName());
+                    return null;
+                }
+                dsFile = file;
+                dsCurrentLine = 0;
             }
-            dsFile = file;
-            dsCurrentLine = 1;
+        }
+        catch(IOException e) {
+            System.out.println("File passed in does not exist");
+            return null;
         }
 
         try(Scanner scnr = new Scanner(file)){
+            if(!(scnr.hasNextLine() && scnr.nextLine().trim().equals(Book.csvHeader.trim()))){
+                System.out.printf("File \"%10s\"'s header is invalid\n", file.getName());
+                return null;
+            };
+
             int i = 0;
 
             //at least has 1(header)
@@ -100,10 +118,18 @@ public class Book
                 String[] bookStats = line.split(",");
                 dsCurrentLine++;
                 if(bookStats.length == 4) {
-                    return new Book(bookStats[0], bookStats[1], bookStats[2], Integer.parseInt(bookStats[3]));
+                    int yearReleased;
+                    try{
+                         yearReleased = Integer.parseInt(bookStats[3]);
+                    }
+                    catch(NumberFormatException e) {
+                        System.out.println("Error parsing year released");
+                        return null;
+                    }
+                    return new Book(bookStats[0].trim(), bookStats[1].trim(), bookStats[2].trim(), yearReleased);
                 }
                 else {
-                    System.out.printf("Corrupted entry within \"%10s\"\n",file.getName());
+                    System.out.printf("Invalid number of properties in a row in file \"%10s\"\n",file.getName());
                     return null;
                 }
             }
--- src/test/java/org/Book/BookTest.java
+++ src/test/java/org/Book/BookTest.java
@@ -1,18 +1,172 @@
 package org.Book;
 
 import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BookTest {
-    @Test
-    void name() {
-        Book book = new Book("Sdsa", "s", "e",22);
+    File makeNewFile(String csvName){
+        File file = new File(csvName);
+        if(file.exists()){
+            file.delete();
+        }
+        return file;
     }
+
     @Test
-    void author() {
+    void comparingSameBooks(){
+        Book book1 = new Book("Pride and Prejudice", "Jane Austen", "Romance Novel", 1813);
+        Book alsoBook1 = new Book("Pride and Prejudice", "Jane Austen", "Romance Novel", 1813);
+        assertTrue(book1.equals(alsoBook1));
+    }
+
+    @Test
+    void comparingDifferentBooks(){
+        Book book1 = new Book("Pride and Prejudice", "Jane Austen", "Romance Novel", 1813);
+        Book alsoBook1 = new Book("Pride with Prejudice", "Jane Austen", "Romance Novel", 1813);
+        assertFalse(book1.equals(alsoBook1));
+    }
+
+
+    //Tests singular instance of serialization
+    //a header should be made as well as one entry
+    @Test
+    void testingSerialization() {
+        File file = makeNewFile("books.csv");
+        Book mb = new Book("To Kill a MockingBird", "Harper Lee", "Thriller",1960);
+        Book.serialize(mb, file);
+        List<String> csvEntries;
+        try {
+            csvEntries = Files.readAllLines(file.toPath());
+        }
+        catch (IOException e) {
+            System.out.println("Something went wrong when reading the file, aborting test");
+            return;
+        }
+        assertEquals(Book.getFormat(), csvEntries.get(0));
+        assertEquals("To Kill a MockingBird,Harper Lee,Thriller,1960", csvEntries.get(1));
+        file.delete();
+    }
+
+    //tests one instance of deserialization
+    @Test
+    void testDeserialization() {
+        File file = makeNewFile("books.csv");
+        Book mb = new Book("To Kill a MockingBird", "Harper Lee", "Thriller",1960);
+        Book.serialize(mb, file);
+        Book mbCpy = Book.deserialize(file);
+        assertTrue(mb.equals(mbCpy));
+        file.delete();
+    }
+
+    //nothing should be added to file
+    @Test
+    void badFileNameToSerialize(){
+        File file = makeNewFile("books.cst");
+        Book mb = new Book("To Kill a MockingBird", "Harper Lee", "Thriller",1960);
+        Book.serialize(mb, file);
+        assertEquals(0, file.length());
+    }
+
+    @Test
+    void giveCSVDirectoryNameToSerialize(){
+        File file = makeNewFile("books.csv");
+        file.mkdir();
+        Book mb = new Book("To Kill a MockingBird", "Harper Lee", "Thriller",1960);
+        Book.serialize(mb, file);
+        file.delete();
+    }
+
+    @Test
+    void deserializeFromInvalidFileNames(){
+
+        //Nonexistent File
+        File nonExistentFile = makeNewFile("books.csv");
+        assertEquals(null, Book.deserialize(nonExistentFile));
+
+        //Directory
+        File dir = makeNewFile("books");
+        dir.mkdir();
+        assertEquals(null,Book.deserialize(dir));
+
+        //wrong file extension
+        File invFileExt = makeNewFile("books.txt");
+        assertEquals(null,Book.deserialize(invFileExt));
+
+        nonExistentFile.delete();
+        dir.delete();
+        invFileExt.delete();
 
     }
+
     @Test
-    void price() {
+    void deserializeWithInvalidHeader(){
+        File exampleCSV = makeNewFile("books.csv");
+        try(FileWriter fw = new FileWriter(exampleCSV)){
+            fw.write("Author,Genre,Year,Title"+"\n");
+            fw.write("Jane Austen,Romance Novel,1891,Pride and Prejudice"+"\n");
+        }
+        catch(IOException e){
+            System.out.println("Something went wrong when writing to the file, aborting test");
+        }
+
+        assertEquals(null,Book.deserialize(exampleCSV));
+        exampleCSV.delete();
+
 
     }
+
+    @Test
+    void deserializeWithInvalidEntries(){
+        File exampleCSV = makeNewFile("books.csv");
+
+        //invalid year
+        try(FileWriter fw = new FileWriter(exampleCSV)){
+            fw.write("title,author,genre,year"+"\n");
+            fw.write("Jane Austen,Romance Novel,1891,Pride and Prejudice"+"\n");
+            fw.flush();
+            assertNull(Book.deserialize(exampleCSV));
+        }
+        catch(IOException e){
+            System.out.println("Something went wrong when writing to the file, aborting test");
+        }
+
+        //not enough
+        try(FileWriter fw = new FileWriter(exampleCSV)){
+            //erase file and restart
+            fw.write("title,author,genre,year"+"\n");
+            fw.write("Pride and Prejudice, Jane Austen, Romance Novel"+"\n");
+            fw.flush();
+            assertNull(Book.deserialize(exampleCSV));
+        }
+        catch(IOException e){
+            System.out.println("Something went wrong when writing to the file, aborting test");
+        }
+
+        //no data found
+        try(FileWriter fw = new FileWriter(exampleCSV)){
+            //erase file and restart
+            fw.write("title,author,genre,year"+"\n");
+            fw.flush();
+            assertNull(Book.deserialize(exampleCSV));
+        }
+        catch(IOException e){
+            System.out.println("Something went wrong when writing to the file, aborting test");
+        }
+
+        exampleCSV.delete();
+
+
+    }
+
+
+
+
+
 }
